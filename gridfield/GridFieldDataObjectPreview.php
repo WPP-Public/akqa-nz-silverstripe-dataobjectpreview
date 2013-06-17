de0be32a0f01111266cfc934dffa2ee9e975916f
Cam Spiers <camspiers@gmail.com>
2013-06-17 15:17:15 +1200

    UPDATE: Start of module

---
+++ gridfield/GridFieldDataObjectPreview.php
@@ -0,0 +1,29 @@
+<?php
+
+class GridFieldDataObjectPreview implements GridField_ColumnProvider
+{
+    public function augmentColumns($gridField, &$columns)
+    {
+
+    }
+
+    public function getColumnsHandled($gridField)
+    {
+
+    }
+
+    public function getColumnContent($gridField, $record, $columnName)
+    {
+
+    }
+
+    public function getColumnAttributes($gridField, $record, $columnName)
+    {
+
+    }
+
+    public function getColumnMetadata($gridField, $columnName)
+    {
+
+    }
+}
